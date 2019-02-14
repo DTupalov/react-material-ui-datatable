5dc2558f74844014ff1df7794ae72625bf19f819
Dmitriy Tupalov <d.tupalov@MacBook-Pro-Dmitrij.local>
2019-02-14 11:51:31 +0300

    chore: update config for output bundle file

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { uglify } from 'rollup-plugin-uglify';
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/bundle.js',
+    file: 'dist/index.js',
     format: 'cjs',
   },
   plugins: [
